8f5820156a4c400a7ebb8d82fd9ffbdb67fb0173
Sakuta <110976118+SakutaTHZ@users.noreply.github.com>
2023-12-17 22:20:52 +0630

    titletext modified

--- js/titleTextGenerator.js
+++ js/titleTextGenerator.js
@@ -1,53 +1,76 @@
-const letterInLine = 12
-const textBox = document.querySelector('.titleTextP')
-let textToGenerate = "@Hi my name is ➡Thar htet Zan➡"
-var words = textToGenerate.split(" ");
-console.log(words);
-// for (let i in textToGenerate) {
-//     textToGenerate[i]==' '?textBox.innerHTML += `<span style="--s:${letterInLine}" class="blank">${textToGenerate[i]}</span>`:textBox.innerHTML += `<span style="--s:${letterInLine}">${textToGenerate[i]}</span>`
-// }
+const letterInLine = 12;
+const textBox = document.querySelector(".titleTextP");
+let textToGenerate = "These days, launching applications means navigating an endless sea of complexity.";
+let sentences = ["✌HI my name is ➡thar htet zan⬅","I am a full stack developer ✌ working as a freelancer✅","You can check✔ my ❤social accounts for more detail informations"]
 
-let wordCount = 0
-let sentence_flg = 0
-let timeCounter = 0
-let sentence = ""
-for (let i = 0; i < words.length; i++) {
-    // console.log(`Word[${i}] - ${words[i]}`);
-    let estimate = words[i].length + wordCount
-    // console.log(`Estimate - ${estimate}\nCount - ${wordCount}`)
-    if(estimate <= letterInLine){
-        for (const key in words[i]) {
-            sentence+=words[i][key]
-        }
-        if(estimate!=letterInLine){
-            wordCount = estimate+1
-            sentence+=" "
-        }else{
-            wordCount = estimate
-        }
+addText(sentences[0])
+
+let num = 1
+setInterval(() => {
+    textBox.innerHTML = ""
+    console.log(num)
+    addText(sentences[num])
+    if(num==sentences.length-1){
+        num = 0 
     }else{
-        sentence_flg += 1
-        // reset
-        wordCount = 0
-        i = i -1
+        num++
+    }
+}, 6000);
+
+function addText(textToGenerate) {
+  var words = textToGenerate.split(" ");
+  console.log(words);
+
+  let wordCount = 0;
+  let sentence_flg = 0;
+  let timeCounter = 0;
+  let sentence = "";
+  for (let i = 0; i < words.length; i++) {
+    // console.log(`Word[${i}] - ${words[i]}`);
+    let estimate = words[i].length + wordCount;
+    // console.log(`Estimate - ${estimate}\nCount - ${wordCount}`)
+    if (estimate <= letterInLine) {
+      for (const key in words[i]) {
+        sentence += words[i][key];
+      }
+      if (estimate != letterInLine) {
+        wordCount = estimate + 1;
+        sentence += " ";
+      } else {
+        wordCount = estimate;
+      }
+    } else {
+      sentence_flg += 1;
+      // reset
+      wordCount = 0;
+      i = i - 1;
     }
 
-    if (sentence_flg == 1 || i == words.length -1) {
-        console.log(`${sentence_flg}: set sentence`)
-        // generate text
-        if(sentence.length!=letterInLine){
-            for (let i = 0; i <= letterInLine-sentence.length; i++) {
-                sentence.length%2==0?sentence+= " ":sentence = " " + sentence
-            }
+    if (sentence_flg == 1 || i == words.length - 1) {
+      console.log(
+        `${sentence.length} : ${letterInLine} [${
+          letterInLine - sentence.length
+        }]`
+      );
+      let toadd = letterInLine - sentence.length;
+      // generate text
+      if (sentence.length != letterInLine) {
+        for (let i = 0; i < toadd; i++) {
+          Math.floor(Math.random() * 2) == 0
+            ? (sentence += " ")
+            : (sentence = " " + sentence);
         }
-        
-        for (let i in sentence) {
-            timeCounter = Math.round((timeCounter+=0.05) * 10) / 10
-            sentence[i]==' '?textBox.innerHTML += `<span style="--s:${letterInLine}; --delay:${timeCounter}s" class="blank">${sentence[i]}</span>`:textBox.innerHTML += `<span style="--s:${letterInLine}; --delay:${timeCounter}s">${sentence[i]}</span>`
-        }
-        sentence = ""
-        sentence_flg = 0
+      }
+
+      for (let i in sentence) {
+        sentence[i] == " "
+          ? (textBox.innerHTML += `<span style="--s:${letterInLine};" class="blank">${sentence[i]}</span>`)
+          : (textBox.innerHTML += `<span style="--s:${letterInLine};">${sentence[i]}</span>`);
+      }
+      sentence = "";
+      sentence_flg = 0;
     }
 
-    console.log(`Sentence - ${sentence}`)
+    // console.log(`Sentence - ${sentence}`)
+  }
 }
