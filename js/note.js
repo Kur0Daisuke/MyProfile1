19d589dac8da0d56cdba68607e1f2d7de80ed03c
Sakuta <110976118+SakutaTHZ@users.noreply.github.com>
2023-12-24 23:41:47 +0630

    added piano sounds

---
+++ js/note.js
@@ -0,0 +1,37 @@
+let currentAudio = 1;
+function PlayNote() {
+    let audioTarget = document.querySelector(`audio[data-key="${currentAudio}"]`)
+    audioTarget.play();
+
+    
+    if(currentAudio+1 > 17) {
+        currentAudio = 1;
+    }else {
+        currentAudio++;
+    }
+}   
+
+fetch('./data/data.json')
+    .then((response) => response.json())
+    .then((json) => {
+        let parent = document.querySelector(".scrollInfo");
+        let parent2 = document.querySelector(".scrollInfo2");
+        function addSocials() {
+            let div = document.createElement("div")
+            for (let i = 0; i < json.socials.length; i++) {
+                div.innerHTML += `<span class="noteable" onclick="PlayNote()"><a class="social" style="--dataColor:${json.socials[i].color};" href="#"><ion-icon name="${json.socials[i].icon}"></ion-icon>${json.socials[i].name}</a></span>`
+            }
+            parent.appendChild(div)
+        }
+        function addLanguage() {
+            let div = document.createElement("div")
+            for (let i = 0; i < json.languages.length; i++) {
+                div.innerHTML += `<span class="noteable" onclick="PlayNote()"><a class="social" style="--dataColor:${json.languages[i].color};" href="#"><ion-icon name="${json.languages[i].icon}"></ion-icon>${json.languages[i].name}</a></span>`
+            }
+            parent2.appendChild(div)
+        }
+        addLanguage()
+        addLanguage()
+        addSocials()
+        addSocials()
+    });
--- main.js
+++ main.js
@@ -46,31 +46,6 @@ function moveProject(place){
     console.log(place + currentProject)
 }
 
-fetch('./data/data.json')
-    .then((response) => response.json())
-    .then((json) => {
-        let parent = document.querySelector(".scrollInfo");
-        let parent2 = document.querySelector(".scrollInfo2");
-        function addSocials() {
-            let div = document.createElement("div")
-            for (let i = 0; i < json.socials.length; i++) {
-                div.innerHTML += `<span><a class="social" style="--dataColor:${json.socials[i].color};" href="#"><ion-icon name="${json.socials[i].icon}"></ion-icon>${json.socials[i].name}</a></span>`
-            }
-            parent.appendChild(div)
-        }
-        function addLanguage() {
-            let div = document.createElement("div")
-            for (let i = 0; i < json.languages.length; i++) {
-                div.innerHTML += `<span><a class="social" style="--dataColor:${json.languages[i].color};" href="#"><ion-icon name="${json.languages[i].icon}"></ion-icon>${json.languages[i].name}</a></span>`
-            }
-            parent2.appendChild(div)
-        }
-        addLanguage()
-        addLanguage()
-        addSocials()
-        addSocials()
-    });
-
 
 // get all social 
 
